767553a13452a5d2a9a2de0960ead817ab427896
Yann Klein <yann.klein@me.com>
2024-11-05 10:47:31 +0900

    lecture done

--- javascript/application.js
+++ javascript/application.js
@@ -1,76 +1,71 @@
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 
-console.log("Hello from JavaScript!");
-
-const url = "https://api.github.com/users/littlebobert";
-
+// console.log("Hello from JavaScript!");
 
 // FETCH GET REQUEST example
-// fetch(url).then((response) => {
-//   console.log(response);
-//   console.log(response.status);
-//   // response.json() converts raw JSON file into JS code (array/object)
-//   // response.json() returns a Promise
-//   return response.json()
-// }).then((userInfo) => {
-//   console.log(userInfo.name);
-// });
-// console.log("after the fetch");
+const url = "https://api.github.com/users/yannklein";
 
+// const response = fetch(url);
+// console.log(response);
 fetch(url)
   .then(response => response.json())
-  .then((userInfo) => {
-    console.log(userInfo.name);
-});
+  .then((data) => {
+    // console.log(data);
+  });
 
-// {
-//   "email": "eve.holt@reqres.in",
-//   "password": "pistol"
-// }
 
 // FETCH POST REQUEST example
 const postUrl = "https://reqres.in/api/register"
 
-// 1. Select elements (2 inputs, form)
-const email = document.querySelector("#email");
-const password =  document.querySelector("#password");
+// 1. Select elements (inputs, form)
 const form = document.querySelector("#form");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
 
 // 2. Listen to a submit on the form
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
   console.log(event);
-  // 2.5 Fetch the reqres API (POST)
+  event.preventDefault();
+  
+  // 2.5 Fetch API (POST)
   const data = {
     email: email.value,
     password: password.value
   }
-  fetch(postUrl, {
+
+  const options = {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data) // converts JS data into JSON file
-  }).then((response) => {
-    if (response.status === 200) {
-      // show a success alert
+    headers: { "Content-type": "application/json"},
+    body: JSON.stringify(data)
+  };
+
+  fetch(postUrl, options)
+  .then((response) => {
+    if (response.ok) {
+      // display a "connected!"
       Swal.fire({
-        position: "top-end",
         icon: "success",
-        title: "Your work has been saved",
+        title: "Successfully connected!",
         showConfirmButton: false,
         timer: 1500
       });
     } else {
-      // show a error alert
+      // display error message
       Swal.fire({
         title: 'Error!',
-        text: 'Do you want to continue',
+        text: 'Try again!',
         icon: 'error',
         confirmButtonText: 'Cool'
       })
     }
+    return response.json();
   })
-})
-
+  .then((data) => {
+    // 3. Change the DOM (console.log)
+    console.log(data);
+  })
+  
+});
 
 
 
